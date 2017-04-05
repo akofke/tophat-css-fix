19516c66e5155a7bdd929cc309459b49cf16d70d
Alex Kofke <akofke@gmail.com>
2017-04-05 19:34:22 -0400

    add alternative with highlighting

---
+++ tophat-highlight.js
@@ -0,0 +1,14 @@
+javascript:(function () {
+    $('<style>')
+        .prop('type', 'text/css')
+        .html('.question_description code {padding: 0; display: inline !important;}')
+        .appendTo('head');
+
+    $('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/default.min.css">').appendTo('head');
+    $.getScript("//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/highlight.min.js", function() {
+        hljs.configure({'languages':['java']});
+        $('.question_description code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        })
+    });
+}());
